c3b8afaa528308c6a3d909db0a3e0ab7a814a889
shashika-himaruwani <sendhimaruwani@gmail.com>
2026-03-15 20:36:32 +0530

    update controller

--- src/main/java/com/example/MathruAI_BackEnd/controller/healthrecords/HealthRecordController.java
+++ src/main/java/com/example/MathruAI_BackEnd/controller/healthrecords/HealthRecordController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import com.example.MathruAI_BackEnd.service.impl.healthrecords.FileUploadService;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;

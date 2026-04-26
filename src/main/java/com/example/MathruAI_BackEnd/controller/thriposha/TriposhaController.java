49fc9864aaceda75eaac86dcf144ecc6c29a203f
Hirushi2001 <118071341+Hirushi2001@users.noreply.github.com>
2026-04-26 20:40:43 +0530

    add triposha tracking

---
+++ src/main/java/com/example/MathruAI_BackEnd/controller/thriposha/TriposhaController.java
@@ -0,0 +1,48 @@
+package com.example.MathruAI_BackEnd.controller.thriposha;
+
+import com.example.MathruAI_BackEnd.dto.triposhaDto.TriposhaDTO;
+import com.example.MathruAI_BackEnd.entity.triposha.TriposhaRecord;
+import com.example.MathruAI_BackEnd.service.interservice.thriposha.TriposhaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/triposha")
+@CrossOrigin
+public class TriposhaController {
+
+    private final TriposhaService service;
+
+    public TriposhaController(TriposhaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public TriposhaRecord create(@RequestBody TriposhaDTO dto) {
+        return service.createRecord(dto);
+    }
+
+    @GetMapping("/midwife/{midwifeId}")
+    public List<TriposhaRecord> getByMidwife(@PathVariable Long midwifeId) {
+        return service.getByMidwife(midwifeId);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.ok("Deleted successfully");
+    }
+
+    @PutMapping("/{id}")
+    public TriposhaRecord update(@PathVariable Long id, @RequestBody TriposhaDTO dto) {
+        return service.update(id, dto);
+    }
+
+    @GetMapping("/patient/{patientId}")
+    public List<TriposhaRecord> getByPatient(@PathVariable Long patientId) {
+        return service.getByPatient(patientId);
+    }
+}
